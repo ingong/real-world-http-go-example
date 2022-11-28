9f5460f3776c1fd7caa284fbacba191da1a805e8
ingong <insong.lee@webtoonscorp.com>
2022-11-28 18:14:38 +0900

    add 3.5 get client

--- client/index.go
+++ client/index.go
@@ -4,17 +4,15 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
-	resp, err := http.Get("http://localhost:18888?")
-	if err != nil {
-		panic(err)
+	values := url.Values{
+		"query": {"hello world"},
 	}
+	resp, _ := http.Get("http://localhost:18888?" + values.Encode())
 	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
+	body, _ := io.ReadAll(resp.Body)
 	log.Println(string(body))
 }
