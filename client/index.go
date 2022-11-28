3118b35efdeec3ec63063bfd35592cb30ff8a7be
ingong <insong.lee@webtoonscorp.com>
2022-11-28 20:42:15 +0900

    add 3.10 cookie

--- client/index.go
+++ client/index.go
@@ -1,36 +1,33 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"log"
-	"mime/multipart"
 	"net/http"
-	"os"
+	"net/http/cookiejar"
+	"net/http/httputil"
 )
 
 func main() {
-	var buf bytes.Buffer                // 멀티파트부를 조립한 뒤 바이트열을 저장할 버퍼를 선언
-	writer := multipart.NewWriter(&buf) // 멀티파트를 조합할 writer를 만든다
-	writer.WriteField("name", "John")   // 파일 이외의 필드는 WriteField() 메서드로 등록한다
-
-	// 파일을 읽는 조작.
-	fileWriter, err := writer.CreateFormFile("file", "photo.jpg") // 개별 파일을 써넣을 io.Writer를 만든다.
-	if err != nil {
-		panic(err)
-	}
-	file, err := os.Open("photo.jpg")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	io.Copy(fileWriter, file) // 파일의 모든 콘텐츠를 io.Writer에 복사
-	writer.Close()
-
-	resp, err := http.Post("http://localhost:18888", writer.FormDataContentType(), &buf)
+	jar, err := cookiejar.New(nil) // 쿠키를 저장할 cookiejar 인스턴스를 만든다
 	if err != nil {
 		panic(err)
 	}
 
-	log.Println("Status:", resp.Status)
+	client := http.Client{ // 쿠키를 저장할 수 있는 http.Client 인스턴스 생성
+		Jar: jar,
+	}
+
+	for i := 0; i < 2; i++ { // 첫 번째 접근에서 쿠키를 받고, 두 번째 이후의 액세스에서 쿠키를 보내는 구조이므로 두 번 접근한다
+		resp, err := client.Get("http://localhost:18888/cookie") // http.Get() 대신 작성한 클라이언트의 Get() 메서드로 액세스
+		if err != nil {
+			panic(err)
+		}
+
+		dump, err := httputil.DumpResponse(resp, true)
+		if err != nil {
+			panic(err)
+		}
+
+		log.Println(string(dump))
+	}
 }
